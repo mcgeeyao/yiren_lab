af974fd1a98ede8dcfc28df81edae9952e1afb8d
alex391a <a.zalex0.9@gmail.com>
2020-09-19 01:45:04 +0800

    9/18 commit

--- myweb/asgi.py
+++ myweb/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from dj_static import Cling
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myweb.settings')
 
-application = get_asgi_application()
+application = Cling(get_asgi_application())
