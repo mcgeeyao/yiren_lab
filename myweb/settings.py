5e551a67dcb73c8cc3f5287ecb72f62555802b3b
alex391a <a.zalex0.9@gmail.com>
2020-09-19 01:49:19 +0800

    9/18 commit

--- myweb/settings.py
+++ myweb/settings.py
@@ -47,7 +47,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            'hosts': [os.environ.get('REDIS_URL')],
+            'hosts': [('localhost',6397)],
         },
     },
 }
