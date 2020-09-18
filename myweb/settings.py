e5d0e73461ae9fa4633821521f0117e89ef73504
alex391a <a.zalex0.9@gmail.com>
2020-09-19 01:04:22 +0800

    9/18 commit

--- myweb/settings.py
+++ myweb/settings.py
@@ -47,7 +47,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            'hosts': [('yiren-lab.herokuapp.com')],
+            'hosts': [('yiren-lab.herokuapp.com',6379)],
         },
     },
 }
