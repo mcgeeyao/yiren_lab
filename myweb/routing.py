81029c18f3634c68acf3cff753a0c20df09a42fb
alex391a <a.zalex0.9@gmail.com>
2020-09-19 14:42:22 +0800

    9/18 commit

--- myweb/routing.py
+++ myweb/routing.py
@@ -1,13 +1,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import OriginValidator,AllowedHostsOriginValidator
 import mainsite.routing
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
+    'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(
         URLRouter(
             mainsite.routing.websocket_urlpatterns
         )
-    ),
+    )),
 })
 
 
