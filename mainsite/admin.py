f42339d9db5626d49063e8a22e41432c81efca21
alex391a <a.zalex0.9@gmail.com>
2021-04-16 18:02:03 +0800

    4/16

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -9,7 +9,7 @@ class UserAdmin(admin.ModelAdmin):
 class bWeekAdmin(admin.ModelAdmin):
     list_display =['week','num','sco','stu']
 
-'''
+
 admin.site.register(Post,PostAdmin)
 admin.site.register(User,UserAdmin)
 admin.site.register(biweekly,bWeekAdmin)
@@ -19,9 +19,8 @@ admin.site.register(Teacher)
 admin.site.register(chatmes)
 
 admin.site.register(kagglefile)
-#admin.site.register(kagscore)
+admin.site.register(kagscore)
 admin.site.register(kagscore1)
 admin.site.register(kagscore2)
 admin.site.register(kagscore3)
 admin.site.register(kagscore4)
-'''
--- mainsite/models.py
+++ mainsite/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-'''
+
 class Post(models.Model):
     title=models.CharField(max_length=200)
     slug=models.CharField(max_length=200)
@@ -168,4 +168,3 @@ class chatmes(models.Model):
         return self.text
     def __str__(self):
         return self.text
-'''
