2a94da6ca57e54a83c02d708703b9fd10f3a173d
alex391a <a.zalex0.9@gmail.com>
2021-04-21 15:17:17 +0800

    4/21

--- mainsite/cron.py
+++ mainsite/cron.py
@@ -1,4 +1,3 @@
-from . import models
 from .models import Team
 
 
--- myweb/wsgi.py
+++ myweb/wsgi.py
@@ -14,5 +14,6 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myweb.settings')
-
+import django
+django.setup()
 application = Cling(get_wsgi_application())
